3d937b01836051c4b3edd1c6f256247c1e1f1155
Matheus <mattvieira123@gmail.com>
2022-08-13 22:42:46 -0300

    common.mk: build blur
    Signed-off-by: Matheus <mattvieira123@gmail.com>

--- common.mk
+++ common.mk
@@ -150,6 +150,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor:64 \
     vendor.qti.hardware.btconfigstore@2.0.vendor:64
 
+# Blur
+TARGET_USES_BLUR := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
