143d8a9a47cc81a8ba8f3a16f8995f175d1bc54a
Matheus <mattvieira123@gmail.com>
2023-03-26 01:19:40 +0000

    sm8150-common: welcome Bootleggers

--- common.mk
+++ common.mk
@@ -36,7 +36,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-yaap
+    $(LOCAL_PATH)/overlay-bootleg
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# YAAP Device Settings
+# Device Settings
 PRODUCT_PACKAGES += \
     DeviceSettings
 
